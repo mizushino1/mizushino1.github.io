eb83c749262c5761062a1a42b25dde9a16eafeba
mizushino1 <jolosantos098@gmail.com>
2026-04-27 11:33:35 +0800

    added a side view of current hero being showcased

--- midterms/dota2/assets/javascript/findhero.js
+++ midterms/dota2/assets/javascript/findhero.js
@@ -1,5 +1,9 @@
-const heroName = document.getElementById("heroName");
-
+const attributeIcons = {
+    str: { attributeIcon: `./assets/image/stricon.webp`},   
+    agi: { attributeIcon: `./assets/image/agiicon.webp`},    
+    int: { attributeIcon: `./assets/image/inticon.webp`},  
+    all: { attributeIcon: `./assets/image/uniicon.webp` }
+};
 
 
 const getDota2Data = async () => {
@@ -17,11 +21,50 @@ const getDota2Data = async () => {
         fetch(abilitiesUrl)
     ]);
 
+    
+
+    const heroID = 1;
+
     const heroesData = await heroesDataRes.json();
     const heroStats = await heroStatsRes.json();
     const heroAbilities = await heroAbilitiesRes.json();
     const abilities = await abilitiesRes.json();
 
+    function getHeroStats(a) {
+        return heroStats.find(h => h.id === a);
+    };
+
+    function getHero(a) {
+        return heroesData.find(h => h.id === a);
+    };
+
+    const currentHero = getHero(heroID);
+    const currentHeroStats = getHeroStats(heroID);
+    const currentHeroImage = baseHeroAssetsUrl + currentHeroStats.img;
+    const currentHeroName = currentHero.localized_name;
+    const currentHeroAttr = currentHeroStats.primary_attr
+    const attrColor = attributeColors[currentHeroAttr].attributeColor;
+    const attrIcon = attributeIcons[currentHeroAttr].attributeIcon;
+
+
+    const elCurrentHeroImage = document.getElementById("currentHeroImage");
+    elCurrentHeroImage.src = currentHeroImage;
+
+    const elCurrentHeroName = document.getElementById("currentHeroName");
+    elCurrentHeroName.innerHTML = currentHeroName;
+
+    const heroStyle = elCurrentHeroName.style;
+
+    const elCurrentHeroAttribute = document.getElementById("attribute");
+    elCurrentHeroAttribute.innerHTML = `<p class="ms-1 d-inline">Primary Attribute:</p><p class="d-inline"> ${currentHeroAttr.toUpperCase()}</p> <img src="${attrIcon}">`
+    
+    
+    heroStyle.backdropFilter = "blur(10px)";
+    heroStyle.webkitBackdropFilter = "blur(10px)"; // For Safari support
+    heroStyle.background = attrColor;
+    heroStyle.border = "2px solid rgba(255, 255, 255, 0.1)";
+    heroStyle.borderRadius = "8px";
+    heroStyle.padding = "4px 16px";
 
 
 }
