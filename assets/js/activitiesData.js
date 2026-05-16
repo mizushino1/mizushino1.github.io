f9ef5f8c191553ffd2b372bd86bf4ef4dee98ea1
mizushino1 <jolosantos098@gmail.com>
2026-05-16 10:24:57 +0800

    added scripting to activities section

---
+++ assets/js/activitiesData.js
@@ -0,0 +1,38 @@
+activitiesData = [
+    {
+        title: "A02",
+        preview: "./assets/images/a02.png",
+        link: "https://mizushino1.github.io/A02/index.html",
+        documentation: "https://github.com/mizushino1/mizushino1.github.io/tree/main/A02",
+        description: `Activity 02 for web dev that showcases a
+                                            specific
+                                            electronic device.`
+
+    },
+    {
+        title: "Midterms",
+        preview: "./assets/images/midterms.png",
+        link: "https://mizushino1.github.io/midterms/dota2",
+        documentation: "https://github.com/mizushino1/mizushino1.github.io/tree/main/midterms/dota2",
+        description: `Midterms for web dev that uses a free public
+                                            API.`
+
+    },
+    {
+        title: "Coming soon..",
+        preview: "./assets/images/placeholder.jpg",
+        link: "#",
+        documentation: "#",
+        description: `Just a placeholder.`
+
+    },
+    {
+        title: "Coming soon..",
+        preview: "./assets/images/placeholder.jpg",
+        link: "#",
+        documentation: "#",
+        description: `Just a placeholder.`
+
+    },
+    
+]
---
+++ assets/js/activitiesLoader.js
@@ -0,0 +1,49 @@
+const activitiesContainer = document.getElementById("activitiesContainer");
+
+function loadActivities() {
+
+    for (i = 0; i != activitiesData.length; i++) {
+        let isLessThan2 = i < 1;
+        let isLessThan3 = i < 2;
+
+        activitiesContainer.innerHTML += `
+        <div class="col-12 col-md-5 card mx-auto ${isLessThan2 ? "" : "my-4"} ${isLessThan3 ? "my-md-0" : "collapse activities"} p-0 glass-card-hover">
+                                    <img src="${activitiesData[i].preview}" class="card-img-top activity-image expandable-img"
+                                        data-bs-target="#toggleModalImage" data-bs-toggle="modal" alt="a02" ">
+                                    <div class="card-body">
+                                        <h5 class="card-title fs-fluid-md">${activitiesData[i].title}</h5>
+                                        <p class="card-text fs-fluid-sm">${activitiesData[i].description}</p>
+                                        <a href="${activitiesData[i].link}"
+                                            class="btn btn-dark border">
+                                            <img src="./assets/images/view.png" class="invert"
+                                                style="height:16px; width: 16px;" alt="">
+                                            <span class="fs-fluid-xs">View</span>
+                                        </a>
+                                        <a href="${activitiesData[i].documentation}"
+                                            class="btn btn-dark custom-btn border">
+                                            <img src="./assets/images/documentation.png" class="invert"
+                                                style="height:16px; width: 16px;" alt="">
+                                            <span class="fs-fluid-xs">Documentation</span>
+                                        </a>
+                                    </div>
+                                </div>
+        
+        `
+
+    }
+    setTimeout(() => {
+        const activityImages = document.querySelectorAll(".activity-image");
+        activityImages.forEach(img => {
+            img.onclick = (event) => {
+                const clickedSrc = event.target.src;
+                modalImg.setAttribute("src", clickedSrc);
+            };
+        });
+
+    }, 10);
+
+
+
+}
+
+loadActivities();
